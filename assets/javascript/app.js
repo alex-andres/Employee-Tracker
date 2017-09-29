2aab3d46724ee4615b81c3da9bd247bf38230ec6
Ashley <you@example.com>
2017-09-28 20:21:23 -0700

    updating js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-// Initialize Firebase
+$(document).ready(function(){
+
   var config = {
     apiKey: "AIzaSyBWG42WDqMRUxnkqyVTW3LCKJXQf-2bWuE",
     authDomain: "employee-tracker-e1f25.firebaseapp.com",
@@ -7,6 +8,7 @@
     storageBucket: "employee-tracker-e1f25.appspot.com",
     messagingSenderId: "1090492013925"
   };
+
   firebase.initializeApp(config);
 
    var database = firebase.database();
@@ -20,8 +22,43 @@
 
    
 
+$("button").on("click", function(){
+	name = $("#employeeName").val().trim();
+	role = $("#role").val().trim();
+	startDate = $("#trainTime").val().trim();
+	// monthsWorked = $("#trainTime").val().trim()
+	// monthlyRate = $("#monthlyRate").val().trim()
+	// totalbilled = $("#employeeName").val().trim()
+
+	database.ref().push({
+		name: name,
+		role: role,
+		startDate: startDate
+	})
+
+});
+
+database.ref().on("child_added", function(childSnapshot){
+	var newRow = $("<tr>");
+
+	var employee = $("<td>").html($("#employeeName").val().trim());
+	// var title = $("<td>").html($("#title").val().trim());
+	var startDate = $("<td>").html($("#trainTime").val().trim());
+	var monthlyRate = $("<td>").html($("#monthlyRate").val().trim());
+	// var totalComp = $("<td>").html($("#totalComp").val().trim());
+
+	newRow.append(employee)
+		// .append(title)
+		.append(startDate)
+		.append(monthlyRate)
+		// .append(totalComp);
+
+	$("table").append(newRow);
+
+})
 
 
+})
 
 
 
