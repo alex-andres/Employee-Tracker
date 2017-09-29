1119377844cef74e5d7c14952abe368fd8f869ab
c-andres <charlesalexanderandres@gmail.com>
2017-09-28 19:48:26 -0700

    add initialization of firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,18 @@
-console.log("Hello World");
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBWG42WDqMRUxnkqyVTW3LCKJXQf-2bWuE",
+    authDomain: "employee-tracker-e1f25.firebaseapp.com",
+    databaseURL: "https://employee-tracker-e1f25.firebaseio.com",
+    projectId: "employee-tracker-e1f25",
+    storageBucket: "employee-tracker-e1f25.appspot.com",
+    messagingSenderId: "1090492013925"
+  };
+  firebase.initializeApp(config);
+
+   var database = firebase.database();
+
+   
+
 
 
 
